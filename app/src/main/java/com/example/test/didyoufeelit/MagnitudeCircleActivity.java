19beb3066f8c8c444774a158c2c921dbea30115b
gayathrikolluru510 <gayathrikolluru@yahoo.com>
2017-12-27 16:15:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/test/didyoufeelit/MagnitudeCircleActivity.java
@@ -0,0 +1,13 @@
+package com.example.test.didyoufeelit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MagnitudeCircleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.magnitude_circle);
+    }
+}
